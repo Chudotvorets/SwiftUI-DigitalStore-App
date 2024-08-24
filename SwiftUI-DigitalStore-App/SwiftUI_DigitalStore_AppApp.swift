343eca72e28d6b1b3842e35535a6a53418e27c4d
Chudotvorets <simonovevgvlad@gmail.com>
2024-08-24 15:59:54 +0400

    Initial Commit

---
+++ SwiftUI-DigitalStore-App/SwiftUI_DigitalStore_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DigitalStore_AppApp.swift
+//  SwiftUI-DigitalStore-App
+//
+//  Created by dev on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DigitalStore_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
